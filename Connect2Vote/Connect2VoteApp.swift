4ab705e8978db8e3fd24400a54e037cea7fd5d06
Ananya Garg <89492329+ananyakgarg@users.noreply.github.com>
2022-10-22 19:14:04 -0400

    Add files via upload
    initial commit

---
+++ Connect2Vote/Connect2VoteApp.swift
@@ -0,0 +1,31 @@
+//
+//  Connect2VoteApp.swift
+//  Connect2Vote
+//
+//  Created by Ananya Garg on 10/22/22.
+//
+
+import SwiftUI
+import FirebaseCore
+
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+  func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    FirebaseApp.configure()
+
+    return true
+  }
+}
+
+
+
+@main
+struct Connect2VoteApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Connect2Vote/ContentView.swift
@@ -0,0 +1,29 @@
+//
+//  ContentView.swift
+//  Connect2Vote
+//
+//  Created by Ananya Garg on 10/22/22.
+//
+
+import SwiftUI
+import MapKit
+
+struct ContentView: View {
+    var body: some View {
+        VStack{
+        }
+    }
+}
+
+
+
+
+
+
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
